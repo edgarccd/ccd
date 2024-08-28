f1c1576551f9ebdcee199f59afd2dd9e2520e82c
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-08-28 10:00:30 -0600

    nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,10 +1,10 @@
 <x-layouts.app>
     <br>
-    <div id="cNosotros" class="container" >
+    <div class="container-sm text-center align-items-center align-items-center"style="box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.3);background-color: white;margin:auto;" >
      
-        <div id="icNosotros">
-            <div id="icuNosotros"></div>
-            <div id="icdNosotros"><small>  
+        <div class="row align-items-center align-items-center" >
+            <div> <img src="/images/ccdb.jpg" alt="CCD" class="img-fluid" style="background-color: white;width:100%"></div>
+            <div  style="background-color: white;  padding: 30px; text-align: justify;width:100%"><small>  
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;La creación de la Unidad Académica de UTJ en la Ciudad Creativa Digital (CCD) constituye una de las acciones
                     estratégicas prioritarias para la Secretaría de Innovación Ciencia y Tecnología (SICyT) del Gobierno del Estado de Jalisco y tiene el propósito de fomentar la creatividad,
                     el aprendizaje y el flujo de ideas entre académicos, estudiantes, emprendedores y empresarios.
