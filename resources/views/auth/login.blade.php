1b93add8b3afa1e65c6a3fd934674d4aa0804464
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-08-28 08:54:01 -0600

    edit login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,27 +2,24 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <br>
-        <div class="input-group mb-3">
-            <span class="input-group-text"
-                id="inputGroup-sizing-default">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Correo</span>
-            <input id="email" type="email" name="email" :value="old('email')" required class="form-control"
-                aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            <x-input-error :messages="$errors->get('email')" />
+<br>
+        <div class="form-floating mb-3">
+            <input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="email">Correo Electrónico</label>
+         <x-input-error :messages="$errors->get('email')" />
         </div>
 
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="inputGroup-sizing-default">Contraseña</span>
-            <input id="password" type="password" name="password" required autocomplete="current-password"
-                class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            <x-input-error :messages="$errors->get('password')" />
+        <div class="form-floating mb-3">
+            <input id="password" type="password" name="password" required autocomplete="current-password" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="password">Contraseña</label>
+         <x-input-error :messages="$errors->get('email')" />
         </div>
-        <br>
+
         <div>
             @if (Route::has('password.request'))
-                <a class="form-text" href="{{ route('password.request') }}">
-                    {{ __('¿Olvidaste tu Contraseña?') }}
-                </a>
+            <a class="form-text" href="{{ route('password.request') }}">
+                {{ __('¿Olvidaste tu Contraseña?') }}
+            </a>
             @endif
             &nbsp; &nbsp; &nbsp;
             <x-primary-button>
