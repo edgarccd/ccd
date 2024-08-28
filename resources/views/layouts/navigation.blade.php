ddc5a5aafa772309f9d0f3afdfced8249a92e7e0
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-08-28 11:27:31 -0600

    ejes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                         </ul>
                     </li>
                 @endif
-                @if (Auth::user()->tipo_id == 1 || Auth::user()->tipo_id == 3)
+                @if (Auth::user()->tipo_id == 1 || Auth::user()->tipo_id == 6)
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
