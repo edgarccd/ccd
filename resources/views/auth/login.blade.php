0045687c72feb50f3435c9e5be401370d2508ab6
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-08-28 08:45:25 -0600

    new login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,22 +2,27 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <div class="mb-3">
-            <x-input-label for="email" :value="__('Correo Electrónico')" />
-            <x-text-input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <br>
+        <div class="input-group mb-3">
+            <span class="input-group-text"
+                id="inputGroup-sizing-default">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Correo</span>
+            <input id="email" type="email" name="email" :value="old('email')" required class="form-control"
+                aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
             <x-input-error :messages="$errors->get('email')" />
         </div>
-        <div class="mb-3">
-            <x-input-label for="password" :value="__('Constraseña')" />
-            <x-text-input id="password" type="password" name="password" required autocomplete="current-password" />
+
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">Contraseña</span>
+            <input id="password" type="password" name="password" required autocomplete="current-password"
+                class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
             <x-input-error :messages="$errors->get('password')" />
         </div>
         <br>
         <div>
             @if (Route::has('password.request'))
-            <a class="form-text" href="{{ route('password.request') }}">
-                {{ __('¿Olvidaste tu Contraseña?') }}
-            </a>
+                <a class="form-text" href="{{ route('password.request') }}">
+                    {{ __('¿Olvidaste tu Contraseña?') }}
+                </a>
             @endif
             &nbsp; &nbsp; &nbsp;
             <x-primary-button>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,6 +24,7 @@
             </div>
 
         </div>
+        <br>
     </div>
 </body>
 
