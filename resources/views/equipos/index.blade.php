a2dafb425f46cbe8fffb21ef1ac889012aa203d7
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-09-05 13:50:01 -0600

    equipos

--- resources/views/equipos/index.blade.php
+++ resources/views/equipos/index.blade.php
@@ -56,34 +56,12 @@
                         <div class="card-body">
                             <div style="height:50%;">
                                 <h5 class="card-title">{{ $equipo->nombre }}</h5>
-                                <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal"
-                                    data-bs-target="#exampleModal">
-                                    {{ $equipo->proyecto->nombre }}
-                                </button>
-                                <div class="modal fade" id="exampleModal" tabindex="-1"
-                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                                                    {{ $equipo->proyecto->nombre }}</h1>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                {{ $equipo->proyecto->descripcion }}
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-bs-dismiss="modal">Cerrar</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <p class="card-text">{{ $equipo->proyecto->nombre }} </p>
                             </div>
                             <div style="display:flex;flex-flow: row wrap; align-items:center;height:50%;">
                                 <a href="{{ route('equipos.edit', $equipo) }}"
                                     class="btn btn-outline-primary">Editar</a>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+
                                 <form action="{{ route('equipos.destroy', $equipo) }}" method="post">
                                     @csrf
                                     @method('delete')
