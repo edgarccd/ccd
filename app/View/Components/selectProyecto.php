016b730fb1ba399d5a59ef270cfd31c447513865
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-09-18 14:10:32 -0600

    equipos

--- app/View/Components/selectProyecto.php
+++ app/View/Components/selectProyecto.php
@@ -2,10 +2,11 @@
 
 namespace App\View\Components;
 
+use App\Models\Periodo;
+use App\Models\Proyecto;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-use App\Models\Proyecto;
 
 class selectProyecto extends Component
 {
@@ -20,10 +21,17 @@ class selectProyecto extends Component
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View | Closure | string
     {
-        $proyectos = Proyecto::orderBy('nombre')->get();
+
+        $proyectos = Proyecto::whereNOTIn('id', function ($query) {
+            $periodo = Periodo::where('activo', 1)->first();
+            $query->select('proyecto_equipos.proyecto_id')
+                ->from('proyecto_equipos')
+                ->join('grupos', 'grupos.id', '=', 'proyecto_equipos.grupo_id')
+                ->where('grupos.periodo_id', $periodo->id);
+        })->orderBy('nombre')->get();
         return view('components.select-proyecto', ['proyectos' => $proyectos]);
-      
+
     }
 }
