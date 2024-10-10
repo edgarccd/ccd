f9cfb261b8918643c92fce4a3e17f2c1267c242a
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-10-10 13:24:19 -0600

    navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -63,10 +63,10 @@
                         aria-expanded="false">
                         Proyectos
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="{{ route('proyectos.catalogoCompleto') }}">Catálogo Disponible</a></li>
+                    <ul class="dropdown-menu">                        
                         <li><a class="dropdown-item" href="{{ route('ejes.index', Auth::user()) }}">Asignar Ejes</a></li>
                         <li><a class="dropdown-item" href="{{ route('equipos.index', Auth::user()) }}">Registrar Equipos</a></li>
+                        <li><a class="dropdown-item" href="{{ route('proyectos.catalogoCompleto') }}">Catálogo</a></li>
                         <li><a class="dropdown-item" href="{{ route('equipos.registrados', Auth::user()) }}">Concentrado</a></li>                
                     </ul>
                 </li>
