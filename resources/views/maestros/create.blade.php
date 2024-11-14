10f1df2aa6b259adb527a512fa4cecf5199da22a
edgarccd <148394463+edgarccd@users.noreply.github.com>
2024-11-14 07:30:24 -0600

    maestros

--- resources/views/maestros/create.blade.php
+++ resources/views/maestros/create.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
     <br>
-    <div class="major container">
-        <h2>Registrar Maestro</h2>
+    <div class="major container col-8">
+        <h3 class="text-center"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+            </svg> Registrar Maestro</h3>
         <hr>
+
         <form action="{{ route('maestros.store') }}" method="post" class="needs-validation" novalidate>
             @csrf
             @include('maestros.form-fields')
             <hr>
-            <button type="submit" class="btn btn-primary">Enviar</button>
-            <a href="{{ route('maestros.index') }}" class="btn btn-secondary">Regresar</a>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="submit" class="btn btn-primary">Grabar</button>
+                <a href="{{ route('maestros.index') }}" class="btn btn-secondary">Regresar</a>
+            </div>
         </form>
     </div>
 </x-app-layout>
--- resources/views/maestros/index.blade.php
+++ resources/views/maestros/index.blade.php
@@ -3,7 +3,11 @@
         <div class="upper">
             @auth
                 <div class="inside">
-                    <h3>Maestros</h3>
+                    <h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                      </svg>
+                    Maestros</h3>
                 </div>
                 <a href="{{ route('maestros.create') }}" class="btn btn-primary">Registrar</a>
             @endauth
@@ -11,11 +15,24 @@
     </x-slot>
     <main class="container">
         <br>
-        <div class="major container">
+        <div class="major container col-8">
             <div class="table-responsive">
+                <form action="#" method="get">
+                    <div class="input-group mb-3">
+                        <button class="btn btn-outline-secondary" type="submit">Buscar por</button>
+                        <select class="form-select" id="busqueda" name="busqueda" required>
+                            <option value="paterno" selected>Apellido Paterno</option>
+                            <option value="materno">Apellido Materno</option>
+                            <option value="nombre">Nombre</option>
+                        </select>
+                        <input id="nombre" name="nombre" type="text" class="form-control"
+                            aria-label="Text input with segmented dropdown button" placeholder="Nombre del Profesor" required>
+                    </div>
+                </form>
+                <hr>
                 <table class="table table-striped">
                     <thead>
-                        <tr>
+                        <tr><th></th>
                             <th>Apellido Paterno</th>
                             <th>Apellido Materno</th>
                             <th>Nombre</th>                            
@@ -26,6 +43,12 @@
                     <tbody>
                         @foreach ($maestros as $maestro)
                             <tr>
+                                <td> <a href="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+                                    <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                    <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                  </svg></a>
+                                </td>
                                 <td>{{ $maestro->apellido_pat }}</td>
                                 <td>{{ $maestro->apellido_mat }}</td>
                                 <td>{{ $maestro->nombre }}</td>                        
